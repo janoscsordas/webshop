519ac235db92478af9e7bec273612fc8265d7ae0
janoscsordas <janoscsordas072@gmail.com>
2024-08-10 12:01:28 +0200

    base code for messages page

--- frontend/src/routes/admin/_authenticated/dashboard/messages/index.lazy.tsx
+++ frontend/src/routes/admin/_authenticated/dashboard/messages/index.lazy.tsx
@@ -1,5 +1,32 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import CommandMenu from '@/components/dashboard/CommandMenu'
+import { ModeToggle } from '@/components/Mode-Toggle'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { createLazyFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/admin/_authenticated/dashboard/messages/')({
-  component: () => <div>Hello /admin/_authenticated/dashboard/messages/!</div>
+  component: MessagesPage
 })
+
+function MessagesPage() {
+  return (
+    <>
+      <header className='flex justify-between items-center'>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <Link to="/admin/dashboard" className='hover:text-foreground'>Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <Link to='/admin/dashboard/messages' className='hover:text-foreground' >Messages</Link>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="flex items-center flex-shrink-0 align-middle gap-5">
+          <CommandMenu />
+          <ModeToggle />
+        </div>
+      </header>
+    </>
+  )
+}
